be1a4230e935d031d537c0d1b23c7892366f6098
Arun4Rajbhar <aruanamika7275@gmail.com>
2024-01-25 20:55:51 +0530

    adding one file

---
+++ function/printname.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+bool  pop(int a){
+    if(a%2==0){
+        return true;
+
+    }
+    return false;
+    
+}
+void table(int n){
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=n;j++){
+            cout<<i*j<<"\t";
+        }
+        cout<<"\n";
+    }
+
+}
+
+
+
+
+
+int main(){
+    int num=567,num1=20;
+    cout<<pop(num)<<"\n";
+    table(num1);
+    return 87689;
+
+
+
+}
